cc6c1b16255dfe908bf8ab60912f83a89c5f7cd4
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-12 12:24:37 +0800

    update db message
    Please create a new .env file

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const dbConnect = () => {
-  if (!process.env.MONGODB_URI) return console.log(`No Database Connected!`);
+  if (!process.env.MONGODB_URI) return console.log(`No Database Connected! Please create a new .env file`);
   mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => console.log(`Connected to the Database`))
